4d331159b91df3e9960c484df1a9eec43c431028
hamidezrb <hamide.queen@gmail.com>
2024-06-08 17:52:17 +0200

    update account.go and client.go

--- client/client.go
+++ client/client.go
@@ -1,53 +1,84 @@
+// package main
+
+// import (
+// 	"bytes"
+// 	"encoding/json"
+// 	"fmt"
+// 	"math/rand"
+// 	"net/http"
+// 	"os"
+// 	"time"
+// )
+
+// const accountManagerURL = "http://account-manager-service/transaction"
+
+// type TransactionRequest struct {
+// 	Amount float64 `json:"amount"`
+// }
+
+// func sendTransaction(amount float64) error {
+// 	requestBody, err := json.Marshal(TransactionRequest{Amount: amount})
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	resp, err := http.Post(accountManagerURL, "application/json", bytes.NewBuffer(requestBody))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	if resp.StatusCode != http.StatusOK {
+// 		return fmt.Errorf("failed to send transaction: %s", resp.Status)
+// 	}
+
+// 	return nil
+// }
+
+// func main() {
+// 	rand.Seed(time.Now().UnixNano())
+
+// 	for {
+// 		amount := (rand.Float64() * 200) - 100 // Random amount between -100 and 100
+// 		fmt.Printf("Sending transaction of amount: %.2f\n", amount)
+
+// 		err := sendTransaction(amount)
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "Error sending transaction: %v\n", err)
+// 		}
+
+// 		sleepDuration := time.Duration(rand.Intn(10)) * time.Second
+// 		time.Sleep(sleepDuration)
+// 	}
+// }
+
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"math/rand"
 	"net/http"
-	"os"
 	"time"
 )
 
-const accountManagerURL = "http://account-manager-service/transaction"
-
-type TransactionRequest struct {
-	Amount float64 `json:"amount"`
-}
-
-func sendTransaction(amount float64) error {
-	requestBody, err := json.Marshal(TransactionRequest{Amount: amount})
-	if err != nil {
-		return err
-	}
-
-	resp, err := http.Post(accountManagerURL, "application/json", bytes.NewBuffer(requestBody))
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to send transaction: %s", resp.Status)
-	}
-
-	return nil
-}
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
+	client := &http.Client{}
 	for {
-		amount := (rand.Float64() * 200) - 100 // Random amount between -100 and 100
-		fmt.Printf("Sending transaction of amount: %.2f\n", amount)
-
-		err := sendTransaction(amount)
+		amount := rand.Intn(200) - 100 // Random amount between -100 and +100
+		req, err := http.NewRequest("POST", "http://localhost:8080/transaction", nil)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error sending transaction: %v\n", err)
+			fmt.Println("Error creating request:", err)
+			continue
+		}
+		q := req.URL.Query()
+		q.Add("amount", fmt.Sprintf("%d", amount))
+		req.URL.RawQuery = q.Encode()
+
+		_, err = client.Do(req)
+		if err != nil {
+			fmt.Println("Error making request:", err)
 		}
 
-		sleepDuration := time.Duration(rand.Intn(10)) * time.Second
-		time.Sleep(sleepDuration)
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
 	}
 }
--- manager/account.go
+++ manager/account.go
@@ -1,70 +1,107 @@
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"sync"
+// 	"time"
+// )
+
+// type TransactionRequest struct {
+// 	Amount float64 `json:"amount"`
+// }
+
+// type AccountManager struct {
+// 	balance float64
+// 	mu      sync.Mutex
+// 	logFile *os.File
+// }
+
+// func NewAccountManager(logFilePath string) (*AccountManager, error) {
+// 	// file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+// 	// if err != nil {
+// 	// 	return nil, err
+// 	// }
+
+// 	return &AccountManager{
+// 		balance: 0,
+// 		// logFile: file,
+// 	}, nil
+// }
+
+// func (am *AccountManager) handleTransaction(w http.ResponseWriter, r *http.Request) {
+// 	var req TransactionRequest
+
+// 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+// 		http.Error(w, "Invalid request body", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	am.mu.Lock()
+// 	defer am.mu.Unlock()
+
+// 	am.balance += req.Amount
+// 	logEntry := fmt.Sprintf("%s - Transaction: %.2f, New Balance: %.2f\n", time.Now().Format(time.RFC3339), req.Amount, am.balance)
+// 	if _, err := am.logFile.WriteString(logEntry); err != nil {
+// 		http.Error(w, "Failed to log transaction", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.WriteHeader(http.StatusOK)
+// }
+
+// func (am *AccountManager) Start() {
+// 	http.HandleFunc("/transaction", am.handleTransaction)
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
+
+// func main() {
+// 	accountManager, _ := NewAccountManager("/var/log/account_manager.log")
+// 	// if err != nil {
+// 	// 	log.Fatalf("Failed to create account manager: %v", err)
+// 	// }
+// 	// defer accountManager.logFile.Close()
+
+// 	fmt.Println("Account manager started")
+// 	accountManager.Start()
+// }
+
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
+	"strconv"
 	"sync"
 	"time"
 )
 
-type TransactionRequest struct {
-	Amount float64 `json:"amount"`
-}
-
-type AccountManager struct {
-	balance float64
+var (
+	balance int
 	mu      sync.Mutex
-	logFile *os.File
-}
+)
 
-func NewAccountManager(logFilePath string) (*AccountManager, error) {
-	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func handleTransaction(w http.ResponseWriter, r *http.Request) {
+	amountStr := r.URL.Query().Get("amount")
+	amount, err := strconv.Atoi(amountStr)
 	if err != nil {
-		return nil, err
-	}
-
-	return &AccountManager{
-		balance: 0,
-		logFile: file,
-	}, nil
-}
-
-func (am *AccountManager) handleTransaction(w http.ResponseWriter, r *http.Request) {
-	var req TransactionRequest
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		http.Error(w, "Invalid amount", http.StatusBadRequest)
 		return
 	}
 
-	am.mu.Lock()
-	defer am.mu.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
+	balance += amount
 
-	am.balance += req.Amount
-	logEntry := fmt.Sprintf("%s - Transaction: %.2f, New Balance: %.2f\n", time.Now().Format(time.RFC3339), req.Amount, am.balance)
-	if _, err := am.logFile.WriteString(logEntry); err != nil {
-		http.Error(w, "Failed to log transaction", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-}
-
-func (am *AccountManager) Start() {
-	http.HandleFunc("/transaction", am.handleTransaction)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("%s: Transaction of %d, new balance: %d\n", time.Now().Format(time.RFC3339), amount, balance)
+	fmt.Fprintf(w, "Transaction successful, new balance: %d", balance)
 }
 
 func main() {
-	accountManager, err := NewAccountManager("/var/log/account_manager.log")
-	if err != nil {
-		log.Fatalf("Failed to create account manager: %v", err)
-	}
-	defer accountManager.logFile.Close()
-
-	fmt.Println("Account manager started")
-	accountManager.Start()
+	http.HandleFunc("/transaction", handleTransaction)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
