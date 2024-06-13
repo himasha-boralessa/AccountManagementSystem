ad248bf49e703a37ce21394ea2ebbba342092ab7
hamidezrb <hamide.queen@gmail.com>
2024-06-13 17:10:20 +0200

    update account-monitor

--- account_monitor/account_monitor.go
+++ account_monitor/account_monitor.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,47 +9,39 @@ import (
 	"time"
 )
 
-type Account struct {
-	ID      string   `json:"id"`
-	Balance int      `json:"balance"`
-	Logs    []string `json:"logs"`
-}
-
 var (
-	accounts = make(map[string]*Account)
-	mu       sync.Mutex
+    balance int
+    mu      sync.Mutex
 )
 
-func getAccounts(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	defer mu.Unlock()
-	json.NewEncoder(w).Encode(accounts)
+type Transaction struct {
+    Time   string `json:"time"`
+    Amount int    `json:"amount"`
 }
 
-func updateAccount(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	amountStr := r.URL.Query().Get("amount")
-	amount, err := strconv.Atoi(amountStr)
-	if err != nil {
-		http.Error(w, "Invalid amount", http.StatusBadRequest)
-		return
-	}
+func handleTransaction(w http.ResponseWriter, r *http.Request) {
+    amountStr := r.URL.Query().Get("amount")
+    amount, err := strconv.Atoi(amountStr)
+    if err != nil {
+        http.Error(w, "Invalid amount", http.StatusBadRequest)
+        return
+    }
 
-	mu.Lock()
-	defer mu.Unlock()
+    mu.Lock()
+    balance += amount
+    mu.Unlock()
 
-	account, exists := accounts[id]
-	if !exists {
-		account = &Account{ID: id, Logs: []string{}}
-		accounts[id] = account
-	}
-	account.Balance += amount
-	account.Logs = append(account.Logs, fmt.Sprintf("%s: %d", time.Now().Format(time.RFC3339), amount))
-	fmt.Fprintf(w, "Account %s updated, new balance: %d", id, account.Balance)
+    response := map[string]interface{}{
+        "balance": balance,
+        "transaction": Transaction{
+            Time:   time.Now().Format(time.RFC3339),
+            Amount: amount,
+        },
+    }
+    json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-	http.HandleFunc("/accounts", getAccounts)
-	http.HandleFunc("/update", updateAccount)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+    http.HandleFunc("/transaction", handleTransaction)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
